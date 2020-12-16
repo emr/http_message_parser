acf9eb1bf9112af5c62e5bed6df92c402544b1e7
Emre Akinci <eemreakinci@gmail.com>
2020-12-16 13:53:30 +0300

    Update typespecs

--- lib/http_message_parser.ex
+++ lib/http_message_parser.ex
@@ -215,8 +215,8 @@ defmodule HttpMessageParser do
   {:error, :empty_message}
   ```
   """
-  @spec parse_request(binary) :: {:error, atom | {atom, binary}} | {:ok, HttpMessageParser.Request}
+  @spec parse_request(binary) :: {:ok, HttpMessageParser.Request} | {:error, atom | {atom, binary}}
   defdelegate parse_request(message), to: HttpMessageParser.Parser
-  @spec parse_response(binary) :: {:error, atom | {atom, binary}} | {:ok, HttpMessageParser.Response}
+  @spec parse_response(binary) :: {:ok, HttpMessageParser.Response} | {:error, atom | {atom, binary}}
   defdelegate parse_response(message), to: HttpMessageParser.Parser
 end
